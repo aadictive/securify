065150a838867d3caa758cccb3731b195a0a7f3a
Aditya Chaudhari <asc4896@gmail.com>
2024-01-20 16:40:36 -0500

    Update 2.3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Securify: Password Perfection in One Tap.
-          Welcome!!
+          Welcome!!!!
         </p>
       </header>
     </div>
