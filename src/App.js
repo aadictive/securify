525ad00cb013fe5da70ba93ca80531b22f38f4a5
Aditya Chaudhari <asc4896@gmail.com>
2024-01-22 12:55:25 -0500

    Update website home page and add footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre";
+import Footer from "./components/Footer";
 import logo from './logo.svg';
+import './style.css';
 import './App.css';
 
 function App() {
+  const [load, updateLoad] = useState(true);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 2000);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App" d={load ? "no-scroll" : "scroll"}>
+      <Preloader load={load} />
+      <div className="home-section">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Securify: Password Perfection in One Tap.
-          Welcome!!!!
         </p>
+        <p>If you have reached this place! Stay tuned as I am actively working on this project</p>
       </header>
+      </div>
+      <Footer />
+      
     </div>
   );
 }
